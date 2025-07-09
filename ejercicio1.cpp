a99b3a7633180b5ff7a9270cd047952d506d9be9
aquispeg <aquispeg@unjbg.edu.pe>
2025-07-09 17:22:49 -0500

    suma de elementos por encima de la diagonal principal

---
+++ ejercicio1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n;
+    cout<<"ingrese el tamano de la matriz cuadrada: ";
+    cin>>n;
+    int matriz[n][n];
+    cout<<"ingrese los elementos de la matriz"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"elemento ["<<i<<"]["<<j<<"]: ";
+            cin>>matriz[i][j];
+        }
+    }
+    cout<<"matriz ingresada"<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<matriz[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            sum=sum+matriz[i][j];
+        }
+    }
+    cout<<"la suma de los elementos por encima de la diagonal principal es: "<<sum<<endl;
+    return 0;
+
+}
